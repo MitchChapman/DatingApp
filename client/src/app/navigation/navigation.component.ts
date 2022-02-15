11869fa459f1ebdb14b1a2353ff31762428c0c4d
Mitch Chapman <Mitch.Chapman@saggezza.com>
2022-02-15 16:24:26 +0000

    End of section 6: Added an auth guard to the members/lists and messages component paths. Created the standard code for the member-list/member-detail/lists and messages components.

--- client/src/app/navigation/navigation.component.ts
+++ client/src/app/navigation/navigation.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { UserModel } from '../models/user.model';
+import { Router } from '@angular/router';
 import { AccountService } from '../services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
 	selector: 'app-navigation',
@@ -11,25 +11,31 @@ import { AccountService } from '../services/account.service';
 export class NavigationComponent implements OnInit {
 
   model: any = {};
+  id = 1
 
-	constructor(public accountService: AccountService) {}
+	constructor(public accountService: AccountService, private router: Router, 
+              private toastr: ToastrService) {}
 
 	ngOnInit(): void {
+    this.id = 1
   }
 
   Login() {
     console.log(this.model);
     this.accountService.Login(this.model).subscribe(
       (response) => {
+        this.router.navigate(['members']);
         console.log(response);
       },
       (error) => {
         console.log(error);
+        this.toastr.error(error.error);
       }
     );
   }
 
   Logout() {
     this.accountService.Logout();
+    this.router.navigate(['/']);
   }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../services/account.service';
 
 @Component({
@@ -10,7 +11,7 @@ export class RegisterComponent implements OnInit {
 	model: any = {};
 	@Output() cancelRegister: EventEmitter<boolean> = new EventEmitter();
 
-	constructor(private accountService: AccountService) {}
+	constructor(private accountService: AccountService, private toastr: ToastrService) {}
 
 	ngOnInit(): void {}
 
@@ -22,7 +23,8 @@ export class RegisterComponent implements OnInit {
 				this.Cancel();
 			},
 			(error) => {
-				console.log(error.message);
+				console.log(error.error);
+				this.toastr.error(error.error);
 			}
 		);
 	}
