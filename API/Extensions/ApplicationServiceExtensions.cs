71849ec6f7a32d7b2fe265263818296e78ee3def
Mitch Chapman <Mitch.Chapman@saggezza.com>
2022-02-16 15:40:05 +0000

    End of section 8: Added member/photo data transfer objects which will be populated with information from the database that the user will have returned to them. Added in another layer of abstraction from the Database context which is a repository. This is an interface that defines a set of methods to retrieve information from the database via the database context. Added auto mapper to allow for the mapping of the entity to the DTOs to be automatic.

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -6,9 +7,11 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Extensions {
 
     public static class ApplicationServiceExtensions {
-        
+
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration) {
-             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options => {
                 options.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
             });
